79e2e67dcd6a86cca8c022655a9d369bc9f6d579
sedani.ab <sedaniarjun@gmail.com>
2018-02-03 12:47:02 +0530

    commit 3

---
+++ app/src/main/java/com/example/aishnaagrawal/ardemo/activity/MyLocation.java
@@ -0,0 +1,337 @@
+package com.example.aishnaagrawal.ardemo.activity;
+
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationProvider;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import com.example.aishnaagrawal.ardemo.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class MyLocation extends AppCompatActivity implements com.example.aishnaagrawal.ardemo.activity.LocationProvider.LocationCallback, GoogleMap.OnInfoWindowClickListener, OnMapReadyCallback{
+
+    private static final LatLng RAJKOT = new LatLng(22.3039, 70.8022);
+    private static final LatLng BANGALORE = new LatLng(12.9716, 77.5946);
+    private static LatLng srcltglng;
+    private static LatLng destltglng;
+
+    /*public static final String TAG = MapsActivity.class.getSimpleName();*/
+
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    private com.example.aishnaagrawal.ardemo.activity.LocationProvider mLocationProvider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_location_demo);
+        //DRAW PATH
+        /*String url = getDirectionsUrl(RAJKOT,BANGALORE);
+        ReadTask downloadTask = new ReadTask();
+        downloadTask.execute(url);*/
+
+        //setUpMapIfNeeded();
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map1);
+        mapFragment.getMapAsync(this);
+
+        mLocationProvider = new com.example.aishnaagrawal.ardemo.activity.LocationProvider(this, this);
+
+        initNavigationDrawer();
+
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+        mLocationProvider.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mLocationProvider.disconnect();
+    }
+
+    /**
+     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
+     * installed) and the map has not already been instantiated.. This will ensure that we only ever
+     * call {@link #setUpMap()} once when {@link #mMap} is not null.
+     * <p/>
+     * If it isn't installed {@link SupportMapFragment} (and
+     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
+     * install/update the Google Play services APK on their device.
+     * <p/>
+     * A user can return to this FragmentActivity after following the prompt and correctly
+     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
+     * have been completely destroyed during this process (it is likely that it would only be
+     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
+     * method in {@link #onResume()} to guarantee that it will be called.
+     */
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            // mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap;
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    /**
+     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
+     * just add a marker near Africa.
+     * <p/>
+     * This should only be called once and when we are sure that {@link #mMap} is not null.
+     */
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    public void handleNewLocation(Location location) {
+        //BitmapDescriptor icon1 = BitmapDescriptorFactory.fromResource(R.drawable.arjun);
+        UiSettings settings = mMap.getUiSettings();
+        /*Log.d(TAG, location.toString());*/
+
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+        srcltglng = latLng;
+        //.icon(BitmapDescriptorFactory.fromResource(R.drawable.arjun))
+        MarkerOptions options = new MarkerOptions()
+                .position(latLng)
+                .title("I am here!")
+                .snippet(String.valueOf(latLng))
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+
+        //.icon(vectorToBitmap(R.drawable.arjun, Color.parseColor("#A4C639")));
+
+        mMap.addMarker(options);
+        // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,5));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+        mMap.setTrafficEnabled(true);
+        settings.setZoomControlsEnabled(true);
+        settings.setCompassEnabled(true);
+        mMap.setOnInfoWindowClickListener(this);
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        Toast.makeText(this, "Info window clicked",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        //RAJKOT TO MY LOCATION ANIMATION
+        /*LatLng rajkot = new LatLng(22.3039, 70.8022);
+        //LatLng rajkot = new LatLng(22.3039, 70.8022);
+        mMap.addMarker(new MarkerOptions().position(rajkot).title("Arjun in Rajkot"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(rajkot));*/
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }
+
+    public void onMapSearchmylocation(View view) {
+        UiSettings settings = mMap.getUiSettings();
+        EditText locationSearch = (EditText) findViewById(R.id.editText);
+        String location = locationSearch.getText().toString();
+        List<Address> addressList = null;
+
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            destltglng = latLng;
+            String url = getDirectionsUrl(srcltglng, destltglng);
+            ReadTask downloadTask = new ReadTask();
+            downloadTask.execute(url);
+            MarkerOptions options = new MarkerOptions()
+                    .position(latLng)
+                    .title("I am here!")
+                    .snippet(String.valueOf(latLng))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+
+            //.icon(vectorToBitmap(R.drawable.arjun, Color.parseColor("#A4C639")));
+
+            mMap.addMarker(options);
+            // mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,5));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+            mMap.setTrafficEnabled(true);
+            settings.setZoomControlsEnabled(true);
+            settings.setCompassEnabled(true);
+            mMap.setOnInfoWindowClickListener(this);
+        }
+    }
+
+    private String getDirectionsUrl(LatLng origin, LatLng dest) {
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+        // Sensor enabled
+        String sensor = "sensor=false";
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + sensor;
+        // Output format
+        String output = "json";
+        return "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
+    }
+
+    private class ReadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... url) {
+            String data = "";
+            try {
+                HttpConnection http = new HttpConnection();
+                data = http.readUrl(url[0]);
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            new ParserTask().execute(result);
+        }
+    }
+
+    private class ParserTask extends
+            AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
+
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(
+                String... jsonData) {
+
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
+
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                PathJSONParser parser = new PathJSONParser();
+                routes = parser.parse(jObject);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return routes;
+        }
+
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> routes) {
+            ArrayList<LatLng> points = null;
+            PolylineOptions polyLineOptions = null;
+
+            // traversing through routes
+            for (int i = 0; i < routes.size(); i++) {
+                points = new ArrayList<LatLng>();
+                polyLineOptions = new PolylineOptions();
+                List<HashMap<String, String>> path = routes.get(i);
+
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+
+                    double lat = Double.parseDouble(point.get("lat"));
+                    double lng = Double.parseDouble(point.get("lng"));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                polyLineOptions.addAll(points);
+                polyLineOptions.width(5);
+                polyLineOptions.color(Color.BLUE);
+            }
+
+            mMap.addPolyline(polyLineOptions);
+        }
+    }
+
+    public void initNavigationDrawer() {
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view1);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                int id = menuItem.getItemId();
+
+                switch (id) {
+                    case R.id.nav_first_fragment:
+                        Intent startActivityIntent = new Intent(MyLocation.this, ARActivity.class);
+                        startActivity(startActivityIntent);
+                        MyLocation.this.finish();
+                        break;
+                    case R.id.nav_second_fragment:
+                        //same logic
+                        break;
+                    case R.id.nav_third_fragment:
+                        //same logic
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+}
+
---
+++ app/src/main/java/com/example/aishnaagrawal/ardemo/activity/SplashActivity.java
@@ -0,0 +1,65 @@
+package com.example.aishnaagrawal.ardemo.activity;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.aishnaagrawal.ardemo.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class SplashActivity extends AppCompatActivity {
+    public double currentLatitude,currentLongitude;
+
+    private static final String TAG = SplashActivity.class.getSimpleName();
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(null != actionBar){
+            actionBar.hide();
+        }
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent startActivityIntent = new Intent(SplashActivity.this, MyLocation.class);
+                startActivity(startActivityIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+    public void handleNewLocation(Location location) {
+        //BitmapDescriptor icon1 = BitmapDescriptorFactory.fromResource(R.drawable.arjun);
+
+        Log.d(TAG, location.toString());
+
+        double currentLatitude = location.getLatitude();
+        double currentLongitude = location.getLongitude();
+        LatLng latLng = new LatLng(currentLatitude, currentLongitude);
+
+    }
+
+}
