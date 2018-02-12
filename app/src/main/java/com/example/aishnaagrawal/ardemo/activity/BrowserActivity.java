15f9f8015d8ca9e025f2e8a3091ee6496cbcb905
sedani.ab <sedaniarjun@gmail.com>
2018-02-12 09:42:25 +0530

    version1

--- app/src/main/java/com/example/aishnaagrawal/ardemo/activity/BrowserActivity.java
+++ app/src/main/java/com/example/aishnaagrawal/ardemo/activity/BrowserActivity.java
@@ -35,8 +35,15 @@ public class BrowserActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browser);
-
         WebView htmlWebView = (WebView) findViewById(R.id.webView);
+       /* View decorView = getWindow().getDecorView();*/
+        htmlWebView.setSystemUiVisibility(
+                WebView.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         htmlWebView.setWebViewClient(new CustomWebViewClient());
         //arjun
         WebSettings webSetting = htmlWebView.getSettings();
